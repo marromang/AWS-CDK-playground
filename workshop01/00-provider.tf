0792f909690af54daad51b3a610a96294c2c86e2
Maria Romero <m.romeroangulo@gmail.com>
2019-11-08 19:24:22 +0100

    changes

--- workshop01/00-provider.tf
+++ workshop01/00-provider.tf
@@ -4,6 +4,6 @@
 
 provider "aws" {
   region     = "eu-west-1"
-  access_key = "AKIA2WZ3W7LH4PORW6Z7"
-  secret_key = "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"
+  access_key = " "
+  secret_key = " "
 }
