8a504f66465ca775a7db5bea46f05709d61625a8
Maria Romero <mromeroa@viewnext.com>
2021-03-23 18:22:46 +0100

    añadido control para status y delete

--- deployments.sh
+++ deployments.sh
@@ -19,25 +19,32 @@ if [[ -z "$REPO_NAME" || -z "$SERVICE" || -z "$PARAMETERS_FILE" || -z "$ACTION" 
     echo "Usage: bash deployments.sh repo_name service parameters_file"
 # path completion for each service
 else
-    if [ $SERVICE == "S3" ]; then
-        TEMPLATE_FILE=$TEMPLATE_PATH/20-create-S3.yaml
-        PARAMS_FILE=$PARAMETERS_PATH/s3/$PARAMETERS_FILE
-    elif [ $SERVICE == "EFS" ]; then
-        TEMPLATE_FILE=$TEMPLATE_PATH/30-create-EFS.yaml
-        PARAMS_FILE=$PARAMETERS_PATH/efs/$PARAMETERS_FILE
-    elif [ $SERVICE == "CB" ]; then
-        TEMPLATE_FILE=$TEMPLATE_PATH/35-CodeBuild.yaml
-        PARAMS_FILE=$PARAMETERS_PATH/codebuild/$PARAMETERS_FILE
-    elif [ $SERVICE == "EB" ]; then
-        TEMPLATE_FILE=$TEMPLATE_PATH/40-ElasticBeanstalk.yaml
-        PARAMS_FILE=$PARAMETERS_PATH/beanstalk/$PARAMETERS_FILE
-    elif [ $SERVICE == "CP" ]; then
-        TEMPLATE_FILE=$TEMPLATE_PATH/45-CodePipeline.yaml
-        PARAMS_FILE=$PARAMETERS_PATH/codepipeline/$PARAMETERS_FILE
-    elif [ $SERVICE == "CFR" ]; then
-        TEMPLATE_FILE=$TEMPLATE_PATH/50-CloudFront.yaml
-        PARAMS_FILE=$PARAMETERS_PATH/cloudfront/$PARAMETERS_FILE
+
+    if [ $ACTION == "status" ]; then  
+        aws cloudformation describe-stacks --stack-name pro-$REPO_NAME-$SERVICE-cf --query Stacks[*].StackStatus | cut -d " " -f2
+    elif [ $ACTION == "delete" ]; then 
+        aws cloudformation delete-stack --stack-name pro-$REPO_NAME-$SERVICE-cf
+    else
+        if [ $SERVICE == "S3" ]; then
+            TEMPLATE_FILE=$TEMPLATE_PATH/20-create-S3.yaml
+            PARAMS_FILE=$PARAMETERS_PATH/s3/$PARAMETERS_FILE
+        elif [ $SERVICE == "EFS" ]; then
+            TEMPLATE_FILE=$TEMPLATE_PATH/30-create-EFS.yaml
+            PARAMS_FILE=$PARAMETERS_PATH/efs/$PARAMETERS_FILE
+        elif [ $SERVICE == "CB" ]; then
+            TEMPLATE_FILE=$TEMPLATE_PATH/35-CodeBuild.yaml
+            PARAMS_FILE=$PARAMETERS_PATH/codebuild/$PARAMETERS_FILE
+        elif [ $SERVICE == "EB" ]; then
+            TEMPLATE_FILE=$TEMPLATE_PATH/40-ElasticBeanstalk.yaml
+            PARAMS_FILE=$PARAMETERS_PATH/beanstalk/$PARAMETERS_FILE
+        elif [ $SERVICE == "CP" ]; then
+            TEMPLATE_FILE=$TEMPLATE_PATH/45-CodePipeline.yaml
+            PARAMS_FILE=$PARAMETERS_PATH/codepipeline/$PARAMETERS_FILE
+        elif [ $SERVICE == "CFR" ]; then
+            TEMPLATE_FILE=$TEMPLATE_PATH/50-CloudFront.yaml
+            PARAMS_FILE=$PARAMETERS_PATH/cloudfront/$PARAMETERS_FILE
+        fi
+        # deployment execution
+        aws cloudformation $ACTION-stack  --stack-name pro-$REPO_NAME-$SERVICE-cf --template-body file://$TEMPLATE_FILE --parameters file://$PARAMS_FILE --profile sd  --capabilities CAPABILITY_NAMED_IAM
     fi
-    # deployment execution
-    aws cloudformation $ACTION-stack  --stack-name pro-$REPO_NAME-$SERVICE-cf --template-body file://$TEMPLATE_FILE --parameters file://$PARAMS_FILE --profile sd  --capabilities CAPABILITY_NAMED_IAM
 fi
